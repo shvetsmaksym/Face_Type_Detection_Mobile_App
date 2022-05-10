f9a9386abd699d77197bb6a060abe4511a8ca955
Maksym Shvets <shvets2616@gmail.com>
2022-05-10 21:54:18 +0200

    add show_photo_screeen, prediction_screen, solved problem with sharing variables between screens

--- main.py
+++ main.py
@@ -14,7 +14,38 @@ class MenuScreen(Screen):
     pass
 
 
-class GaleryUploadScreen(Screen):
+class GalleryUploadScreen(Screen):
+    def selected(self, filename):
+        try:
+            self.ids.my_image.source = filename[0]
+        except Exception as e:
+            pass
+
+
+class MakePhotoScreen(Screen):
+    def capture(self):
+        camera = self.ids['camera']
+
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        path = f"saved_images/IMG_{timestr}.png"
+        camera.export_to_png(path)
+        print("Captured")
+
+        time.sleep(1)
+        self.propagate_to_show_screen(path)
+
+    def propagate_to_show_screen(self, path):
+        try:
+            self.manager.get_screen('show_photo').ids.my_image.source = path
+        except Exception as e:
+            pass
+
+
+class ShowPhotoScreen(Screen):
+    pass
+
+
+class PredictionScreen(Screen):
     def selected(self, filename):
         try:
             self.ids.my_image.source = filename[0]
@@ -22,35 +53,24 @@ class GaleryUploadScreen(Screen):
             pass
 
     def predict(self):
-        pred = make_prediction(img_path=self.ids.my_image.source)
-        self.ids.face_type_label.text = pred
-
-
-class TakePhotoScreen(Screen):
-    def capture(self):
-        camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_{}.png".format(timestr))
-        print("Captured")
-
-
-class PredictionScreen(Screen):
-    pass
+        predicted = make_prediction(img_path=self.ids.my_image.source)
+        self.ids.face_type_label.text = predicted
 
 
 class GlassesApp(App):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.predicted_face_type = None
+        self.image_for_predict = None
 
     def build(self):
-        self.predicted_face_type = None
         sm = ScreenManager()
         sm.add_widget(MenuScreen(name='menu'))
-        sm.add_widget(GaleryUploadScreen(name='gallery_upload'))
-        # sm.add_widget(PredictionScreen(name="predict_face_type"))
-        # sm.add_widget(TakePhotoScreen(name='take_photo'))
+        sm.add_widget(GalleryUploadScreen(name='gallery_upload'))
+        sm.add_widget(MakePhotoScreen(name='make_photo'))
+        sm.add_widget(ShowPhotoScreen(name='show_photo'))
+        sm.add_widget(PredictionScreen(name="predict_face_type"))
+
 
         return sm
 
