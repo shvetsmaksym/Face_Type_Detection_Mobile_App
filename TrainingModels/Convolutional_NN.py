7cf067776da5713500d2a8db5b857ddeeea52ca3
Maksym Shvets <shvets2616@gmail.com>
2022-05-31 21:04:59 +0200

    add image preprocessing and modify model

--- TrainingModels/Convolutional_NN.py
+++ TrainingModels/Convolutional_NN.py
@@ -1,57 +1,71 @@
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, SeparableConv2D, Flatten, Dense, BatchNormalization, Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
-from tensorflow.keras.applications.vgg16 import preprocess_input
-import matplotlib.pyplot as plt
-# import cv2
-# import os
-# import numpy as np
+from tensorflow.keras.regularizers import L2
+
 
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
 # HYPERPARAMETERS
-BATCH_SIZE = 16
-PATH_TO_TRAIN_DATA = "Data/training_set/"
-PATH_TO_TEST_DATA = "Data/testing_set/"
+BATCH_SIZE = 32
+PATH_TO_TRAIN_DATA = "Data/Processed/train/"
+PATH_TO_TEST_DATA = "Data/Processed/test/"
+
+
+def scaler(x_array):
+    x_array = (x_array / 255 * 0.8) + 0.1
+    return x_array
 
 
 # PREPARE TRAIN AND TEST SETS
-train = ImageDataGenerator(rescale=1/255, preprocessing_function=preprocess_input, horizontal_flip=True)
-test = ImageDataGenerator(rescale=1/255, preprocessing_function=preprocess_input)
+face_types = {0: "Heart", 1: "Oblong", 2: "Oval", 3: "Round", 4: "Square"}
+X_train, X_test, y_train, y_test = [], [], [], []
 
-train_set = train.flow_from_directory(PATH_TO_TRAIN_DATA,
-                                      target_size=(224, 224),
-                                      class_mode='categorical',
-                                      batch_size=BATCH_SIZE,
-                                      # save_format='png'
-                                      )
-test_set = test.flow_from_directory(PATH_TO_TEST_DATA,
-                                    target_size=(224, 224),
-                                    class_mode='categorical',
-                                    batch_size=BATCH_SIZE,
-                                    # save_format='png'
-                                    )
-X_train, y_train = train_set.next()
-X_test, y_test = test_set.next()
+for y, face_type in face_types.items():
+    for file in os.listdir(PATH_TO_TRAIN_DATA + f'/{face_type}'):
+        img = cv2.imread(PATH_TO_TRAIN_DATA + f'/{face_type}' + f'/{file}', cv2.IMREAD_GRAYSCALE)
+        X_train.append(scaler(img))
+        one_hot_encoding_vector = [0 for _ in range(len(face_types))]
+        one_hot_encoding_vector[y] = 1
+        y_train.append(one_hot_encoding_vector)
+
+for y, face_type in face_types.items():
+    for file in os.listdir(PATH_TO_TEST_DATA + f'/{face_type}'):
+        img = cv2.imread(PATH_TO_TEST_DATA + f'/{face_type}' + f'/{file}', cv2.IMREAD_GRAYSCALE)
+        X_test.append(scaler(img))
+        one_hot_encoding_vector = [0 for _ in range(len(face_types))]
+        one_hot_encoding_vector[y] = 1
+        y_test.append(one_hot_encoding_vector)
+
+X_train, X_test, y_train, y_test = np.array(X_train), np.array(X_test), np.array(y_train), np.array(y_test)
 print("Data preprocessing completed...")
 
 
-# FUNCTION FOR CREATING A CNN MODEL
+# BUILD CNN MODEL
 def build_model():
     """model from Kaggle https://www.kaggle.com/bradwel/facial-classification-model"""
-    input_img = Input(shape=(224, 224, 3), name='ImageInput')
-    x = Conv2D(4, (3, 3), activation='relu', padding='same', name='Conv1_1')(input_img)
+    input_img = Input(shape=(224, 168, 1), name='ImageInput')
+    x = Conv2D(16, (5, 5), activation='relu', kernel_regularizer=L2(0.00001), padding='same', name='Conv1_1')(input_img)
     x = BatchNormalization(name='bn1')(x)
-    x = SeparableConv2D(4, (3, 3), activation='relu', padding='same', name='Conv3_3')(x)
+    x = SeparableConv2D(16, (3, 3), activation='relu', kernel_regularizer=L2(0.00001), padding='same', name='Conv3_3')(x)
     x = MaxPooling2D((2, 2), name='pool1')(x)
 
-    # x = SeparableConv2D(32, (3, 3), activation='relu', padding='same', name='Conv2_1')(x)
-    # x = SeparableConv2D(32, (3, 3), activation='relu', padding='same', name='Conv2_2')(x)
-    # x = MaxPooling2D((2, 2), name='pool2')(x)
+    x = SeparableConv2D(32, (3, 3), activation='relu', kernel_regularizer=L2(0.00001), padding='same', name='Conv2_1')(x)
+    x = BatchNormalization(name='bn3')(x)
+    x = SeparableConv2D(32, (3, 3), activation='relu', kernel_regularizer=L2(0.00001), padding='same', name='Conv2_2')(x)
+    x = MaxPooling2D((2, 2), name='pool2')(x)
+
+    # x = Conv2D(16, (3, 3), activation='relu', kernel_regularizer=L2(0.00001), padding='same', name='Conv4_1')(x)
+    # x = BatchNormalization(name='bn4')(x)
+    # x = Conv2D(16, (3, 3), activation='relu', kernel_regularizer=L2(0.00001), padding='same', name='Conv4_2')(x)
+    # x = MaxPooling2D((2, 2), name='pool3')(x)
     #
     # x = SeparableConv2D(32, (3, 3), activation='relu', padding='same', name='Conv3_1')(x)
     # x = BatchNormalization(name='bn1')(x)
@@ -68,10 +82,10 @@ def build_model():
     # x = MaxPooling2D((2, 2), name='pool4')(x)
 
     x = Flatten(name='flatten')(x)
-    # x = Dense(1024, activation='relu', name='fc1')(x)
-    # x = Dropout(0.7, name='dropout1')(x)
-    x = Dense(8, activation='relu', name='fc2')(x)
-    x = Dropout(0.5, name='dropout2')(x)
+    x = Dense(64, activation='relu', name='fc1')(x)
+    x = Dropout(0.2, name='dropout1')(x)
+    x = Dense(32, activation='relu', name='fc2')(x)
+    x = Dropout(0.2, name='dropout2')(x)
     x = Dense(5, activation='softmax', name='fc3')(x)
 
     model = Model(inputs=input_img, outputs=x)
@@ -84,22 +98,17 @@ print(ConvNet_model.summary())
 
 # Compile model
 adam_optimizer = Adam(learning_rate=0.0001, decay=1e-5)
-# es = EarlyStopping(patience=5, min_delta=0.05, monitor="val_accuracy")
-#chkpt = ModelCheckpoint(filepath='best_model_todate', save_best_only=True, save_weights_only=True)
+es = EarlyStopping(patience=5, min_delta=0.01, monitor="val_accuracy")
+# chkpt = ModelCheckpoint(filepath='best_model_todate', save_best_only=True, save_weights_only=True)
 ConvNet_model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer=adam_optimizer)
 
 # Fit model
-history = ConvNet_model.fit(
-    x=X_train,
-    y=y_train,
-    batch_size=BATCH_SIZE,
-    # steps_per_epoch=train_set.samples//BATCH_SIZE,
-    # validation_split=0.2,
-    validation_data=(X_test, y_test),
-    # validation_steps=(train_set.samples / 5) // BATCH_SIZE,
-    epochs=5
-    # callbacks=[es, chkpt]
-)
+history = ConvNet_model.fit(x=X_train,
+                            y=y_train,
+                            validation_data=(X_test, y_test),
+                            epochs=20,
+                            callbacks=[es]
+                            )
 
 # RESULTS
 acc = history.history['accuracy']
@@ -123,4 +132,4 @@ plt.show()
 
 # SAVE MODEL AND WEIGHTS
 ConvNet_model.save("TrainingModels/models_h5/CNN_2.h5")
-ConvNet_model.save_weights("TrainingModels/models_h5/CNN_2_weights.h5")
+# ConvNet_model.save_weights("TrainingModels/models_h5/CNN_2_weights.h5")
--- TrainingModels/Neural_Net_Prediction.py
+++ TrainingModels/Neural_Net_Prediction.py
@@ -1,21 +1,85 @@
 from tensorflow import keras
+import cv2
 import numpy as np
-from keras_preprocessing.image import ImageDataGenerator, load_img, img_to_array
-from tensorflow.keras.applications.vgg16 import preprocess_input
+
+
+def scaler(x_array):
+    x_array = (x_array * 0.8) + 0.1
+    return x_array
 
 
 def make_prediction(img_path, model_path='TrainingModels\models_h5\CNN_2.h5'):
     classes = {0: "Heart", 1: "Oblong", 2: "Oval", 3: "Round", 4: "Square"}
     model = keras.models.load_model(model_path)
-    img = load_img(img_path, target_size=(224, 224))
-    img = img_to_array(img)
-    img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
-    img = preprocess_input(img)
+    img = cv2.imread(img_path)
+    processed_img = process_image(cv_image=img)
+    if processed_img is not None:
+        processed_img = scaler(processed_img)
+        processed_img = processed_img.reshape((1, processed_img.shape[0], processed_img.shape[1]))
+        # processed_img = preprocess_input(processed_img)
 
-    pred = model.predict(img)
-    return "Your type of face is {}.".format(classes[np.argmax(pred)])
+        pred = model.predict(processed_img)
+        return "Your type of face is {}.".format(classes[np.argmax(pred)])
+    else:
+        print("Can't process image.")
 
+
+def add_lacking_px(image, target_size=(168, 224)):
+    """Add missing pixels to the edges so the image is of target_size.
+    : param image: cv2 image
+    : param target_size: target width and hegth of output image"""
+
+    temp_im = (np.array(image) / 255).astype(np.float32)
+    w, h = target_size
+
+    def solve_px_partition(ax_size, target):
+        if ax_size == target:
+            return (0, 0)
+
+        resid = target - ax_size % target
+
+        if resid % 2 == 0:
+            return resid // 2, resid // 2
+        else:
+            return resid // 2, resid // 2 + 1
+
+    # Define margins to add to the edges.
+    top, down = solve_px_partition(temp_im.shape[0], h)
+    left, right = solve_px_partition(temp_im.shape[1], w)
+
+    px_top = np.ones((top, temp_im.shape[1]))
+    px_down = np.ones((down, temp_im.shape[1]))
+    temp_im = np.vstack((px_top, temp_im, px_down))
+
+    px_left = np.ones((temp_im.shape[0], left))
+    px_right = np.ones((temp_im.shape[0], right))
+    temp_im = np.column_stack((px_left, temp_im, px_right))
+
+    return temp_im
+
+
+def process_image(cv_image):
+
+    try:
+        img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    except Exception as e:
+        return None
+
+    height, width = img.shape[0], img.shape[1]
+    while height > 224 or width > 168:
+        height, width = int(height * 0.5), int(width * 0.5)
+
+    img = cv2.resize(img, (width, height))
+
+    # ADAPTIVE THRESHOLDING
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    thresh = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 3)
+
+    # ADD LACKING PIXELS TO THE EDGES
+    processed = add_lacking_px(thresh, target_size=(168, 224))
+
+    return processed
 
 
 if __name__ == "__main__":
-    make_prediction(img_path='Data/testing_set/Oval/oval (2).jpg')
+    print(make_prediction(img_path='Data/testing_set/Round/round (368).jpg'))
